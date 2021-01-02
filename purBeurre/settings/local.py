ff6b2bbfd57260dc4d9808671106c0a34a79c560
Christophe-Dupin <mail.christophe.dupin@gmail.com>
2021-01-02 20:03:28 +0100

    fix imort issue of base settings module

--- purBeurre/settings/local.py
+++ purBeurre/settings/local.py
@@ -1,19 +1,20 @@
 import os
+
 # Import the base settings
 from .base import *
 from dotenv import find_dotenv, load_dotenv
 
 DEBUG = True
 load_dotenv(find_dotenv())
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALLOWED_HOSTS = []
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ['DATABASE_NAME'],
-        "USER": os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': "localhost",
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
--- purBeurre/settings/production.py
+++ purBeurre/settings/production.py
@@ -1,19 +1,20 @@
 import os
+
 # Import the base settings
-from ./base import *
+from .base import *
 from dotenv import find_dotenv, load_dotenv
 
 DEBUG = False
 load_dotenv(find_dotenv())
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = ['157.245.249.94']
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALLOWED_HOSTS = ["157.245.249.94"]
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv('DATABASE_NAME'),
-        "USER": os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': "localhost",
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
